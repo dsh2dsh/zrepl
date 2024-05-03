35a3f834c021e0dfb35f6741d58d13776ea55cb9
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2024-05-03 20:43:20 +0200

    Do rpc calls with one minute deadline
    Sometimes I see something strange during replication and I can't understand
    what's the reason. For the beginning, I decided to set deadline for rpc calls
    and see how it works.

--- rpc/grpcclientidentity/grpchelper/authlistener_grpc_adaptor_wrapper.go
+++ rpc/grpcclientidentity/grpchelper/authlistener_grpc_adaptor_wrapper.go
@@ -1,8 +1,10 @@
-// Package grpchelper wraps the adaptors implemented by package grpcclientidentity into a less flexible API
-// which, however, ensures that the individual adaptor primitive's expectations are met and hence do not panic.
+// Package grpchelper wraps the adaptors implemented by package
+// grpcclientidentity into a less flexible API which, however, ensures that the
+// individual adaptor primitive's expectations are met and hence do not panic.
 package grpchelper
 
 import (
+	"context"
 	"time"
 
 	"google.golang.org/grpc"
@@ -14,13 +16,15 @@ import (
 	"github.com/zrepl/zrepl/transport"
 )
 
-// The following constants are relevant for interoperability.
-// We use the same values for client & server, because zrepl is more
-// symmetrical ("one source, one sink") instead of the typical
-// gRPC scenario ("many clients, single server")
+// The following constants are relevant for interoperability. We use the same
+// values for client & server, because zrepl is more symmetrical ("one source,
+// one sink") instead of the typical gRPC scenario ("many clients, single
+// server")
 const (
 	StartKeepalivesAfterInactivityDuration = 5 * time.Second
 	KeepalivePeerTimeout                   = 10 * time.Second
+
+	callTimeout = time.Minute
 )
 
 type Logger = logger.Logger
@@ -36,13 +40,9 @@ func ClientConn(cn transport.Connecter, log Logger) *grpc.ClientConn {
 	dialerOption := grpc.WithContextDialer(grpcclientidentity.NewDialer(log, cn))
 	cred := grpc.WithTransportCredentials(
 		grpcclientidentity.NewTransportCredentials(log))
-	// we use context.Background without a timeout here because we don't set
-	// grpc.WithBlock
-	//
-	// => docs: "In the non-blocking case, the ctx does not act against the
-	// connection. It only controls the setup steps."
+	timeout := grpc.WithUnaryInterceptor(WithTimeout(callTimeout, log))
 	cc, err := grpc.NewClient("passthrough:///doesn't matter done by dialer",
-		dialerOption, cred, ka)
+		dialerOption, cred, ka, timeout)
 	if err != nil {
 		log.WithError(err).Error("cannot create gRPC client conn (non-blocking)")
 		// It's ok to panic here: the we call grpc.DialContext without the
@@ -55,8 +55,29 @@ func ClientConn(cn transport.Connecter, log Logger) *grpc.ClientConn {
 	return cc
 }
 
-// NewServer is a convenience interface around the TransportCredentials and Interceptors interface.
-func NewServer(authListener transport.AuthenticatedListener, clientIdentityKey interface{}, logger grpcclientidentity.Logger, ctxInterceptor grpcclientidentity.Interceptor) (srv *grpc.Server, serve func() error) {
+func WithTimeout(t time.Duration, log Logger) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply any,
+		cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption,
+	) error {
+		log = log.WithField("method", method).WithField("timeout", t)
+		if t <= 0 {
+			log.Warn("ignore negative timeout")
+			return invoker(ctx, method, req, reply, cc, opts...)
+		}
+
+		log.Debug("set call timeout")
+		ctx, cancel := context.WithTimeout(ctx, t)
+		defer cancel()
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
+// NewServer is a convenience interface around the TransportCredentials and
+// Interceptors interface.
+func NewServer(authListener transport.AuthenticatedListener,
+	clientIdentityKey interface{}, logger grpcclientidentity.Logger,
+	ctxInterceptor grpcclientidentity.Interceptor,
+) (srv *grpc.Server, serve func() error) {
 	ka := grpc.KeepaliveParams(keepalive.ServerParameters{
 		Time:    StartKeepalivesAfterInactivityDuration,
 		Timeout: KeepalivePeerTimeout,
@@ -66,8 +87,10 @@ func NewServer(authListener transport.AuthenticatedListener, clientIdentityKey i
 		PermitWithoutStream: true,
 	})
 	tcs := grpcclientidentity.NewTransportCredentials(logger)
-	unary, stream := grpcclientidentity.NewInterceptors(logger, clientIdentityKey, ctxInterceptor)
-	srv = grpc.NewServer(grpc.Creds(tcs), grpc.UnaryInterceptor(unary), grpc.StreamInterceptor(stream), ka, ep)
+	unary, stream := grpcclientidentity.NewInterceptors(logger,
+		clientIdentityKey, ctxInterceptor)
+	srv = grpc.NewServer(grpc.Creds(tcs), grpc.UnaryInterceptor(unary),
+		grpc.StreamInterceptor(stream), ka, ep)
 
 	serve = func() error {
 		if err := srv.Serve(netadaptor.New(authListener, logger)); err != nil {
