d6405ae690c08d9459a077116178956855c1bb36
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-01-03 20:03:23 +0100

    Refactor "configcheck" cmd

--- internal/client/configcheck.go
+++ internal/client/configcheck.go
@@ -11,10 +11,8 @@ import (
 	"gopkg.in/yaml.v3"
 
 	"github.com/dsh2dsh/zrepl/internal/cli"
-	"github.com/dsh2dsh/zrepl/internal/config"
 	"github.com/dsh2dsh/zrepl/internal/daemon/job"
 	"github.com/dsh2dsh/zrepl/internal/daemon/logging"
-	"github.com/dsh2dsh/zrepl/internal/logger"
 )
 
 var configcheckArgs struct {
@@ -29,92 +27,61 @@ var ConfigcheckCmd = &cli.Subcommand{
 	ConfigWithIncludes: true,
 
 	SetupFlags: func(f *pflag.FlagSet) {
-		f.StringVar(&configcheckArgs.format, "format", "", "dump parsed config object [yaml|json]")
-		f.StringVar(&configcheckArgs.what, "what", "all", "what to print [all|config|jobs|logging]")
+		f.StringVar(&configcheckArgs.format, "format", "",
+			"dump parsed config object [yaml|json]")
+		f.StringVar(&configcheckArgs.what, "what", "all",
+			"what to print [all|config|jobs|logging]")
 	},
 
-	Run: func(ctx context.Context, subcommand *cli.Subcommand, args []string) error {
-		formatMap := map[string]func(interface{}){
-			"": func(i interface{}) {},
-			"json": func(i interface{}) {
-				if err := json.NewEncoder(os.Stdout).Encode(subcommand.Config()); err != nil {
-					panic(err)
-				}
-			},
-			"yaml": func(i interface{}) {
-				if err := yaml.NewEncoder(os.Stdout).Encode(subcommand.Config()); err != nil {
-					panic(err)
-				}
-			},
-		}
+	Run: func(_ context.Context, subcommand *cli.Subcommand, _ []string) error {
+		return checkConfig(subcommand)
+	},
+}
 
-		formatter, ok := formatMap[configcheckArgs.format]
-		if !ok {
-			return fmt.Errorf("unsupported --format %q", configcheckArgs.format)
-		}
+func checkConfig(subcommand *cli.Subcommand) error {
+	c := subcommand.Config()
+	var hadErr bool
 
-		var hadErr bool
-
-		// further: try to build jobs
-		confJobs, _, err := job.JobsFromConfig(subcommand.Config())
-		if err != nil {
-			err := fmt.Errorf("cannot build jobs from config: %w", err)
-			if configcheckArgs.what == "jobs" {
-				return err
-			} else {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				confJobs = nil
-				hadErr = true
-			}
-		}
-
-		// further: try to build logging outlets
-		outlets, err := logging.OutletsFromConfig(subcommand.Config().Global.Logging)
-		if err != nil {
-			err := fmt.Errorf("cannot build logging from config: %w", err)
-			if configcheckArgs.what == "logging" {
-				return err
-			} else {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				outlets = nil
-				hadErr = true
-			}
-		}
-
-		whatMap := map[string]func(){
-			"all": func() {
-				o := struct {
-					config  *config.Config
-					jobs    []job.Job
-					logging *logger.Outlets
-				}{
-					subcommand.Config(),
-					confJobs,
-					outlets,
-				}
-				formatter(o)
-			},
-			"config": func() {
-				formatter(subcommand.Config())
-			},
-			"jobs": func() {
-				formatter(confJobs)
-			},
-			"logging": func() {
-				formatter(outlets)
-			},
-		}
-
-		wf, ok := whatMap[configcheckArgs.what]
-		if !ok {
-			return fmt.Errorf("unsupported --format %q", configcheckArgs.what)
-		}
-		wf()
-
-		if hadErr {
-			return errors.New("config parsing failed")
+	// further: try to build logging outlets
+	_, err := logging.OutletsFromConfig(c.Global.Logging)
+	if err != nil {
+		err := fmt.Errorf("cannot build logging from config: %w", err)
+		if configcheckArgs.what == "logging" {
+			return err
 		} else {
-			return nil
+			fmt.Fprintln(os.Stderr, err)
+			hadErr = true
 		}
-	},
+	}
+
+	// further: try to build jobs
+	_, _, err = job.JobsFromConfig(c)
+	if err != nil {
+		err := fmt.Errorf("cannot build jobs from config: %w", err)
+		if configcheckArgs.what == "jobs" {
+			return err
+		} else {
+			fmt.Fprintln(os.Stderr, err)
+			hadErr = true
+		}
+	}
+
+	switch configcheckArgs.format {
+	case "":
+	case "json":
+		if err := json.NewEncoder(os.Stdout).Encode(c); err != nil {
+			return fmt.Errorf("failed encode to json: %w", err)
+		}
+	case "yaml":
+		if err := yaml.NewEncoder(os.Stdout).Encode(c); err != nil {
+			return fmt.Errorf("failed encode to yaml: %w", err)
+		}
+	default:
+		return fmt.Errorf("unsupported --format %q", configcheckArgs.format)
+	}
+
+	if hadErr {
+		return errors.New("config parsing failed")
+	}
+	return nil
 }
