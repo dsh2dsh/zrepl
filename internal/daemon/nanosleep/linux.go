5f9e7b62732645d68d38c4b12f70af083eb24573
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2025-03-24 20:39:19 +0100

    Make wrapcheck linter happy
      Error: internal/daemon/nanosleep/linux.go:10:9: error returned from external package is unwrapped: sig: func golang.org/x/sys/unix.ClockNanosleep(clockid int32, flags int, request *golang.org/x/sys/unix.Timespec, remain *golang.org/x/sys/unix.Timespec) (err error) (wrapcheck)
      	return unix.ClockNanosleep(clockid, flags, request, remain)

--- internal/daemon/nanosleep/linux.go
+++ internal/daemon/nanosleep/linux.go
@@ -4,6 +4,7 @@ package nanosleep
 
 import "golang.org/x/sys/unix"
 
+//nolint:wrapcheck // do not wrap unix err
 func clockNanosleep(clockid int32, flags int, request *unix.Timespec,
 	remain *unix.Timespec,
 ) error {
